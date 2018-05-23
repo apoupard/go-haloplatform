f5c5c87698aa4bba63f309e6e712719d66a0d682
shadowcodex <jsduncan98@gmail.com>
2018-05-22 19:40:59 -0500

    update address for rolling raft test

--- params/config.go
+++ params/config.go
@@ -36,7 +36,7 @@ var (
 
 var (
 	// The address for receiving MN reward
-	MasterNodeRewardAddress          = common.HexToAddress("0xee3c8095994d59e5601016a0316e4e607054d58c")
+	MasterNodeRewardAddress          = common.HexToAddress("0x75163e545e32e775f5a2b0b7b8a1455cb1ac7eb3")
 	MasterNodeReward        *big.Int = big.NewInt(0) // 38e+18 is too big for initializing, setting it later in makeConfigNode()
 	MasterNodeRewardString           = "38000000000000000000"
 )
